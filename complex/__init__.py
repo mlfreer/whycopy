157e9f36f3d63b9369cd95f7e6e3c1e7c310cba1
mlfreer <1991F@inbox.ru>
2022-02-18 12:21:38 +0000

    creating complex treatment

---
+++ complex/__init__.py
@@ -0,0 +1,39 @@
+from otree.api import *
+
+
+doc = """
+Your app description
+"""
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'complex'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    pass
+
+#--------------------------------------------
+# PAGES
+#--------------------------------------------
+class DecisionScreen(Page):
+    pass
+
+#--------------------------------------------
+
+
+
+#--------------------------------------------
+# PAGE SEQUENCE
+#--------------------------------------------
+page_sequence = [DecisionScreen]
