f8e57b3b838327eab890c227f2fce7457e91d20d
mlfreer <1991F@inbox.ru>
2022-05-19 16:49:12 +0100

    supplementary files

---
+++ simple_instructions/__init__.py
@@ -0,0 +1,34 @@
+from otree.api import *
+
+
+doc = """
+Your app description
+"""
+
+
+class C(BaseConstants):
+    NAME_IN_URL = 'simple_instructions'
+    PLAYERS_PER_GROUP = None
+    NUM_ROUNDS = 1
+
+
+class Subsession(BaseSubsession):
+    pass
+
+
+class Group(BaseGroup):
+    pass
+
+
+class Player(BasePlayer):
+    pass
+
+
+# PAGES
+class Instructions(Page):
+    pass
+
+
+
+
+page_sequence = [Instructions]
